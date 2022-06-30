a5276006c53bcea8ea23e457661230c62f9da63b
pranta-barua007 <prantabarua247@gmail.com>
2022-06-30 22:39:15 +0600

    updated some comments

--- src/app.js
+++ src/app.js
@@ -5,15 +5,24 @@ import helmet from "helmet";
 
 const app = express(); //intializing express to app constant
 
-app.use(helmet()); // secutiy middleware
+app.use(helmet()); // security middleware
 app.use(
+  // cross origin middleware
   cors({
     origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   })
 );
-app.use(morgan("combined"));
-app.use(express.json()); //middleware function to decode JSON req
+app.use(morgan("combined")); // logging middleware
+app.use(express.json()); // middleware function to decode JSON req
+
+app.get("/", (req, res) => {
+  res.send("Hello from root route");
+});
+
+app.get("/you", (req, res) => {
+  res.send("Hello from you route");
+});
 
 app.get("/me", (req, res) => {
   res.send("Hellooooo");
