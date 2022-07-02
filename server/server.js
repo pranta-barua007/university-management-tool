4d64a27edec9e30b1c5462abf0ad8078800cb849
pranta-barua007 <prantabarua247@gmail.com>
2022-07-03 01:08:11 +0600

    standardization of api in server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   })
 );
