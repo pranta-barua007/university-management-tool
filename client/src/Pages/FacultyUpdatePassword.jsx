517b6f0d12404cee70f697981c45dbe4ec217882
pranta-barua007 <prantabarua247@gmail.com>
2022-07-21 10:19:00 +0600

    updated client to react version 18

--- client/src/Pages/FacultyUpdatePassword.jsx
+++ client/src/Pages/FacultyUpdatePassword.jsx
@@ -90,7 +90,6 @@ const FacultyUpdatePassword = () => {
                         {error.confirmNewPassword}
                       </div>
                     )}
-                    <p></p>
                   </div>
                   <button type="submit" className="btn btn-info btn-block ">
                     Update Password
